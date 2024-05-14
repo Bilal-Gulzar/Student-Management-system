5648c41cc6c263af755a755536b92e3abba2d00a
Bilal-Gulzar <bilalgulzar415@gamil.com>
2024-05-14 15:19:16 +0500

    add validation in name Input & age Input

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import inquirer from "inquirer";
 class Studentinfo {
     name;
     age;
-    isfeePaid;
+    isfeePaid; /// i did not publish this package after changing ID logic unique++;
     ID;
     school;
     course;
@@ -24,36 +24,61 @@ while (condition) {
         {
             name: "nam",
             type: "input",
-            message: " Enter student name"
+            message: " Enter student name",
+            validate: (input) => {
+                if (Number(input)) {
+                    return "Please write your name correctly.";
+                }
+                else if (input == "") {
+                    return "you have to fill it";
+                }
+                else {
+                    return true;
+                }
+            },
         },
         {
             name: "age",
-            type: "number",
-            message: "student age"
+            type: "input",
+            message: "student age",
+            validate: (input) => {
+                if (isNaN(input)) {
+                    return " This is not a number ";
+                }
+                else if (input > 110) {
+                    return "write your correct age";
+                }
+                else if (input == "") {
+                    return "you have to fill it";
+                }
+                else {
+                    return true;
+                }
+            },
         },
         {
             name: "fee",
             type: "list",
             message: "student fee is paid?",
-            choices: ["Yes", "No"]
+            choices: ["Yes", "No"],
         },
         {
             name: "sch",
             type: "list",
             message: "student College Name",
-            choices: ["GIAIC", "PIAIC"]
+            choices: ["GIAIC", "PIAIC"],
         },
         {
             name: "course",
             type: "list",
             message: "Select Student Course",
-            choices: ["Cloud Computing", "Web 3.O", "Meta Verse", "AI", "Block Chain"]
+            choices: ["Cloud Computing", "Web 3.O", "Meta Verse", "AI", "Block Chain"],
         },
         {
             name: "ask",
             type: "confirm",
-            message: "Add more students Data"
-        }
+            message: "Add more students Data",
+        },
     ]);
     UniqueId++;
     let studentData = new Studentinfo(answer.nam, answer.age, answer.fee, UniqueId, answer.sch, answer.course);
--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ import Choice from "inquirer/lib/objects/choice.js";
 class Studentinfo{
     name: string
     age: number
-    isfeePaid:string
+    isfeePaid:string         /// i did not publish this package after changing ID logic unique++;
     ID: number
     school:string
     course:string  
@@ -28,41 +28,59 @@ let UniqueId = 100;
 let condition: boolean = true
 while(condition){
 let answer = await inquirer.prompt([
-    {
-        name:"nam",
-        type: "input",
-        message:" Enter student name"
-
+  {
+    name: "nam",
+    type: "input",
+    message: " Enter student name",
+    validate: (input) => {
+      if (Number(input)){
+        return "Please write your name correctly.";
+      } else if (input == "") {
+        return "you have to fill it";
+      } else {
+        return true;
+      }
     },
-    {
-        name:"age",
-        type:"number",
-        message:"student age"
+  },
+  {
+    name: "age",
+    type: "input",
+    message: "student age",
+    validate: (input) => {
+      if (isNaN(input)){
+        return " This is not a number ";
+      } else if(input > 110){
+        return "write your correct age"
+      }else if (input == ""){
+        return "you have to fill it";
+      } else {
+        return true;
+      }
     },
-    {
-        name:"fee",
-        type:"list",
-        message:"student fee is paid?",
-        choices:["Yes","No"]
-    },
-    {
-        name:"sch",
-        type:"list",
-        message: "student College Name",
-        choices: ["GIAIC","PIAIC"]
-    },
-    {
-        name:"course",
-        type:"list",
-        message:"Select Student Course",
-        choices:["Cloud Computing","Web 3.O","Meta Verse","AI","Block Chain"]
-    },
-    {
-        name:"ask",
-        type:"confirm",
-        message:"Add more students Data"
-
-    }
+  },
+  {
+    name: "fee",
+    type: "list",
+    message: "student fee is paid?",
+    choices: ["Yes", "No"],
+  },
+  {
+    name: "sch",
+    type: "list",
+    message: "student College Name",
+    choices: ["GIAIC", "PIAIC"],
+  },
+  {
+    name: "course",
+    type: "list",
+    message: "Select Student Course",
+    choices: ["Cloud Computing", "Web 3.O", "Meta Verse", "AI", "Block Chain"],
+  },
+  {
+    name: "ask",
+    type: "confirm",
+    message: "Add more students Data",
+  },
 ]);
 UniqueId++;
 let studentData = new Studentinfo(answer.nam,answer.age,answer.fee,UniqueId,answer.sch,answer.course);
